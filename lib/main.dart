49a7f96c84ca5bb34d6c737fe367b68c9ce02e61
AlexPj72 <apujia@gmail.com>
2026-08-11 09:32:52 +0200

    Refactor main.dart to use MultiProvider for dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:myapp/app_router.dart';
 import 'package:myapp/viewmodels/home_viewmodel.dart';
+import 'package:myapp/repositories/appezzamento_repository.dart';
 import 'package:provider/provider.dart';
-
-// Importazioni per Firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  // Garantisce l'inizializzazione dei widget Flutter
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Inizializzazione di Firebase con le opzioni della piattaforma corrente
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Esecuzione del login anonimo in background
   try {
     final auth = FirebaseAuth.instance;
     if (auth.currentUser == null) {
@@ -29,12 +24,14 @@ void main() async {
     debugPrint("❌ Errore durante il login anonimo: $e");
   }
 
-  // Rimuove il carattere '#' dagli URL su Web
   usePathUrlStrategy();
 
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => HomeViewModel(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeViewModel()),
+        ChangeNotifierProvider(create: (_) => AppezzamentoRepository()),
+      ],
       child: const OrtoApp(),
     ),
   );
@@ -42,7 +39,6 @@ void main() async {
 
 class OrtoApp extends StatelessWidget {
   const OrtoApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
