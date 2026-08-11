f8d403f0532d9b79ba8cdc04804dfb7db06d72c9
AlexPj72 <apujia@gmail.com>
2026-08-11 09:32:11 +0200

    Modify Appezzamento model to add regione and userId
    Updated the Appezzamento model to include 'regione' and 'userId' fields, while removing 'comune' as a required parameter in the constructor. Adjusted the fromFirestore and toFirestore methods accordingly.

--- lib/models/appezzamento.dart
+++ lib/models/appezzamento.dart
@@ -5,16 +5,20 @@ class Appezzamento {
   final String nome;
   final double larghezza;
   final double lunghezza;
-  final String comune;
+  final String regione;
   final String provincia;
+  final String comune;
+  final String userId;
 
   Appezzamento({
     required this.id,
     required this.nome,
     required this.larghezza,
     required this.lunghezza,
-    required this.comune,
+    required this.regione,
     required this.provincia,
+    required this.comune,
+    required this.userId,
   });
 
   factory Appezzamento.fromFirestore(DocumentSnapshot doc) {
@@ -24,8 +28,22 @@ class Appezzamento {
       nome: data['nome'] ?? '',
       larghezza: (data['larghezza'] ?? 0.0).toDouble(),
       lunghezza: (data['lunghezza'] ?? 0.0).toDouble(),
-      comune: data['comune'] ?? '',
+      regione: data['regione'] ?? '',
       provincia: data['provincia'] ?? '',
+      comune: data['comune'] ?? '',
+      userId: data['userId'] ?? '',
     );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'nome': nome,
+      'larghezza': larghezza,
+      'lunghezza': lunghezza,
+      'regione': regione,
+      'provincia': provincia,
+      'comune': comune,
+      'userId': userId,
+    };
   }
 }
